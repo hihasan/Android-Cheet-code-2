1cec803d0c5cd267766b6ad9be4bbe7d66564175
hihasan <hasan@hihasan.com>
2018-07-23 16:45:09 +0600

    ConnectivityManager

--- Internet Connectivity/app/src/main/java/com/hihasan/internetconnectivity/MainActivity.java
+++ Internet Connectivity/app/src/main/java/com/hihasan/internetconnectivity/MainActivity.java
@@ -1,13 +1,72 @@
 package com.hihasan.internetconnectivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private WebView webView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        checkConnection();
+    }
+
+    //Check connection Here
+    private void checkConnection()
+    {
+        if(isOnline())
+        {
+            Toast.makeText(getApplicationContext(), "You are connected to Internet", Toast.LENGTH_SHORT).show();
+            webView.setWebViewClient(new MyWebViewClient());
+
+            String url = "http://google.com";
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.loadUrl(url);
+
+
+        }
+
+        else
+        {
+            Toast.makeText(getApplicationContext(), "You are not connected to Internet", Toast.LENGTH_SHORT).show();
+            final Intent mainIntent = new Intent(MainActivity.this, NoInternet.class);
+            MainActivity.this.startActivity(mainIntent);
+            MainActivity.this.finish();
+        }
+    }
+
+    //Internet Connection
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
