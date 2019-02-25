f06b3282c6e6963b6704ef6268c9b1aabbe00720
hihasan <hasan@hihasan.com>
2019-02-25 13:41:22 +0600

    layer and java

--- ExpandableText/app/src/main/java/com/hihasan/expandabletext/MainActivity.java
+++ ExpandableText/app/src/main/java/com/hihasan/expandabletext/MainActivity.java
@@ -2,6 +2,12 @@ package com.hihasan.expandabletext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
+
+import at.blogc.android.views.ExpandableTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ExpandableTextView expandableTextView = (ExpandableTextView) this.findViewById(R.id.expandableTextView);
+        final Button buttonToggle = (Button) this.findViewById(R.id.button_toggle);
+
+        expandableTextView.setAnimationDuration(750L);
+
+        expandableTextView.setInterpolator(new OvershootInterpolator());
+
+        expandableTextView.setExpandInterpolator(new OvershootInterpolator());
+        expandableTextView.setCollapseInterpolator(new OvershootInterpolator());
+
+        buttonToggle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View v){
+                buttonToggle.setText(expandableTextView.isExpanded() ? R.string.expand : R.string.close);
+                expandableTextView.toggle();
+            }
+        });
+
+        buttonToggle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View v){
+                if (expandableTextView.isExpanded()){
+                    expandableTextView.collapse();
+                    buttonToggle.setText(R.string.expand);
+                }else{
+                    expandableTextView.expand();
+                    buttonToggle.setText(R.string.close);
+                }
+            }
+        });
+
     }
 }
