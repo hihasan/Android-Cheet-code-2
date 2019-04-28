5c341d62c1304d7bff0df7cc4ab6ad4c1e32c11f
hihasan <hasan@hihasan.com>
2019-04-28 18:28:39 +0600

    submit, retrive,clear

--- sharedpreferences/app/src/main/java/com/hihasan/sharedpreferences/MainActivity.java
+++ sharedpreferences/app/src/main/java/com/hihasan/sharedpreferences/MainActivity.java
@@ -1,15 +1,30 @@
 package com.hihasan.sharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatEditText name, phone, email;
+    AppCompatButton submit,clear,retrive;
+
+    public static final String MyPreferences="hihasan";
+    public static final String Name="name";
+    public static final String Phone="phone";
+    public static final String Email="email";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +33,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        name=(AppCompatEditText) findViewById (R.id.name);
+        phone=(AppCompatEditText) findViewById (R.id.phone);
+        email=(AppCompatEditText) findViewById (R.id.email);
+
+        submit=(AppCompatButton) findViewById (R.id.submit);
+        clear=(AppCompatButton) findViewById (R.id.clear);
+        retrive=(AppCompatButton) findViewById (R.id.retrive);
+        final SharedPreferences sharedPreferences=getSharedPreferences(MyPreferences, Context.MODE_PRIVATE);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String n=name.getText().toString();
+                String ph=phone.getText().toString();
+                String e=email.getText().toString();
+
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+
+                editor.putString(Name,n);
+                editor.putString(Phone,ph);
+                editor.putString(Email,e);
+
+                editor.commit();
+                Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_SHORT).show();
+
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        retrive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //String nValue, phValue,eValue;
+                String nameValue=sharedPreferences.getString(Name,"");
+                String phoneValue=sharedPreferences.getString(Phone,"");
+                String emailValue=sharedPreferences.getString(Email,"");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                name.setText(nameValue);
+                phone.setText(phoneValue);
+                email.setText(emailValue);
+            }
+        });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name.getText().clear();
+                phone.getText().clear();
+                email.getText().clear();
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
+
     }
 }
