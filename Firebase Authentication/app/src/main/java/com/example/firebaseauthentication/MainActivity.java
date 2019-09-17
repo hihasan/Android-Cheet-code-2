7e3c27c9fd8af3b3ed16b110007d4462d854add3
hihasan <hasan@hihasan.com>
2019-09-17 16:25:55 +0600

    details

--- Firebase Authentication/app/src/main/java/com/example/firebaseauthentication/MainActivity.java
+++ Firebase Authentication/app/src/main/java/com/example/firebaseauthentication/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseApp.initializeApp(this);
+//        FirebaseApp.initializeApp(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- Firebase Authentication/app/src/main/java/com/example/firebaseauthentication/ProfileActivity.java
+++ Firebase Authentication/app/src/main/java/com/example/firebaseauthentication/ProfileActivity.java
@@ -1,9 +1,13 @@
 package com.example.firebaseauthentication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity
 {
@@ -11,5 +15,17 @@ public class ProfileActivity extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                startActivity(intent);
+            }
+        });
     }
 }
