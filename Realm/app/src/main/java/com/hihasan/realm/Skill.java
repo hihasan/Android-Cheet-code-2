5b0858ab475841a8268265839cccf696c311f0ab
hihasan <hasan@hihasan.com>
2019-10-01 11:53:18 +0600

    initialize

---
+++ Realm/app/src/main/java/com/hihasan/realm/Skill.java
@@ -0,0 +1,14 @@
+package com.hihasan.realm;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class Skill extends RealmObject
+{
+    public static final String PROPERTY_SKILL="skillName";
+
+    @PrimaryKey
+    @Required
+    public String skillName;
+}
