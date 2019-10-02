3884afdb181834f1e78a580dd8e1adc4128fcfd1
hihasan <hasan@hihasan.com>
2019-10-02 10:37:28 +0600

    realm crud operations

--- Realm/app/src/main/java/com/hihasan/realm/MainRealmActivity.java
+++ Realm/app/src/main/java/com/hihasan/realm/MainRealmActivity.java
@@ -11,6 +11,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import io.realm.Realm;
+import io.realm.RealmList;
+import io.realm.RealmResults;
+import io.realm.exceptions.RealmPrimaryKeyConstraintException;
 
 public class MainRealmActivity extends AppCompatActivity implements View.OnClickListener
 {
@@ -55,25 +58,170 @@ public class MainRealmActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btnAdd:
-                Toast.makeText(getApplicationContext(),"Action Required",Toast.LENGTH_SHORT).show();
+                addEmployee();
                 break;
             case R.id.btnRead:
-                Toast.makeText(getApplicationContext(),"Action required", Toast.LENGTH_SHORT).show();
+                ReadEmployes();
                 break;
             case R.id.btnUpdate:
-                Toast.makeText(getApplicationContext(),"action Required", Toast.LENGTH_SHORT).show();
+                updateEmployeeRecords();
                 break;
             case R.id.btnFilterByAge:
-                Toast.makeText(getApplicationContext(),"Action Required", Toast.LENGTH_SHORT).show();
+                filterByAge();
                 break;
             case R.id.btnDelete:
-                Toast.makeText(getApplicationContext(),"Action Required", Toast.LENGTH_SHORT).show();
+                deleteEmployeeRecord();
                 break;
             case R.id.btnDeleteWithSkill:
-                Toast.makeText(getApplicationContext(),"Action Required", Toast.LENGTH_SHORT).show();
+                deleteEmployeeWithSkill();
                 break;
 
         }
 
+    }
+
+    private void addEmployee() {
+
+        Realm realm = null;
+        try {
+            realm = Realm.getDefaultInstance();
+            realm.executeTransaction(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+
+
+                    try {
+
+
+                        if (!inName.getText().toString().trim().isEmpty()) {
+                            Employee employee = new Employee();
+                            employee.name = inName.getText().toString().trim();
+
+                            if (!inAge.getText().toString().trim().isEmpty())
+                                employee.age = Integer.parseInt(inAge.getText().toString().trim());
+
+
+                            String languageKnown = inSkill.getText().toString().trim();
+
+                            if (!languageKnown.isEmpty()) {
+                                Skill skill = realm.where(Skill.class).equalTo(Skill.PROPERTY_SKILL, languageKnown).findFirst();
+
+                                if (skill == null) {
+                                    skill = realm.createObject(Skill.class, languageKnown);
+                                    realm.copyToRealm(skill);
+                                }
+
+                                employee.skills = new RealmList<>();
+                                employee.skills.add(skill);
+                            }
+
+                            realm.copyToRealm(employee);
+                        }
+
+                    } catch (RealmPrimaryKeyConstraintException e) {
+                        Toast.makeText(getApplicationContext(), "Primary Key exists, Press Update instead", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } finally {
+            if (realm != null) {
+                realm.close();
+            }
+        }
+    }
+
+    private void ReadEmployes(){
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                RealmResults<Employee> results = realm.where(Employee.class).findAll();
+                textView.setText("");
+                for (Employee employee : results) {
+                    textView.append(employee.name + " age: " + employee.age + " skill: " + employee.skills.size());
+                }
+            }
+        });
+    }
+
+    private void updateEmployeeRecords() {
+
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+
+
+                if (!inName.getText().toString().trim().isEmpty()) {
+
+
+                    Employee employee = realm.where(Employee.class).equalTo(Employee.PROPERTY_NAME, inName.getText().toString()).findFirst();
+                    if (employee == null) {
+                        employee = realm.createObject(Employee.class, inName.getText().toString().trim());
+                    }
+                    if (!inAge.getText().toString().trim().isEmpty())
+                        employee.age = Integer.parseInt(inAge.getText().toString().trim());
+
+                    String languageKnown = inSkill.getText().toString().trim();
+                    Skill skill = realm.where(Skill.class).equalTo(Skill.PROPERTY_SKILL, languageKnown).findFirst();
+
+                    if (skill == null) {
+                        skill = realm.createObject(Skill.class, languageKnown);
+                        realm.copyToRealm(skill);
+                    }
+
+
+                    if (!employee.skills.contains(skill))
+                        employee.skills.add(skill);
+
+                }
+            }
+        });
+    }
+
+    private void deleteEmployeeRecord() {
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Employee employee = realm.where(Employee.class).equalTo(Employee.PROPERTY_NAME, inName.getText().toString()).findFirst();
+                if (employee != null) {
+                    employee.deleteFromRealm();
+                }
+            }
+        });
+    }
+
+    private void deleteEmployeeWithSkill() {
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+
+                RealmResults<Employee> employees = realm.where(Employee.class).equalTo("skills.skillName", inSkill.getText().toString().trim()).findAll();
+                employees.deleteAllFromRealm();
+            }
+        });
+    }
+
+
+    private void filterByAge() {
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+
+                RealmResults<Employee> results = realm.where(Employee.class).greaterThanOrEqualTo(Employee.PROPERTY_AGE, 25).findAllSortedAsync(Employee.PROPERTY_NAME);
+
+                txtFilterByAge.setText("");
+                for (Employee employee : results) {
+                    txtFilterByAge.append(employee.name + " age: " + employee.age + " skill: " + employee.skills.size());
+                }
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (realm != null) {
+            realm.close();
+        }
     }
 }
