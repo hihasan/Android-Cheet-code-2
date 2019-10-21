40a2b7d6863837989499a21be3a3e79b81b69809
hihasan <hasan@hihasan.com>
2019-10-21 18:18:09 +0600

    tag 2

--- TagViewOnEditText/app/src/main/java/com/hihasan/tagview2/MainActivity.java
+++ TagViewOnEditText/app/src/main/java/com/hihasan/tagview2/MainActivity.java
@@ -6,50 +6,68 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import me.originqiu.library.EditTag;
+
+public class MainActivity extends AppCompatActivity implements SwitchCompat.OnCheckedChangeListener {
+
+    private EditTag editTagView;
+
+    private SwitchCompat statusSwitchView;
+
+    private List<String> tagStrings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        editTagView = (EditTag) findViewById(R.id.edit_tag_view);
+        statusSwitchView = (SwitchCompat) findViewById(R.id.status_switch);
+        statusSwitchView.setOnCheckedChangeListener(this);
+
+        for (int i = 0; i < 10; i++) {
+            tagStrings.add("test" + i);
+        }
+        //Set tag add callback before set tag list
+        editTagView.setTagAddCallBack(new EditTag.TagAddCallback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onTagAdd(String tagValue) {
+                if ("test1".equals(tagValue)) {
+                    return false;
+                } else {
+                    return true;
+                }
             }
         });
+        editTagView.setTagDeletedCallback(new EditTag.TagDeletedCallback() {
+            @Override
+            public void onTagDelete(String deletedTagValue) {
+                Toast.makeText(MainActivity.this, deletedTagValue, Toast.LENGTH_SHORT).show();
+            }
+        });
+        editTagView.setTagList(tagStrings);
+
+        editTagView.addTag("hello world!");
+        editTagView.removeTag("test3");
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        editTagView.setEditable(isChecked);
     }
 }
