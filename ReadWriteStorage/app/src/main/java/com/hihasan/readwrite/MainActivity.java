e5ba1543af1e3cdae6817096484068a92e87fb7a
hihasan <hasan@hihasan.com>
2019-10-28 15:20:23 +0600

    folder

--- ReadWriteStorage/app/src/main/java/com/hihasan/readwrite/MainActivity.java
+++ ReadWriteStorage/app/src/main/java/com/hihasan/readwrite/MainActivity.java
@@ -1,5 +1,9 @@
 package com.hihasan.readwrite;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,12 +11,38 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+    EditText inputText;
+    TextView response;
+    Button saveButton, readButton;
+    Context context=this;
+
+    private String FILENAME = "SampleFile.txt";
+    private String DNAME = "Hihasan";
+    File myExternalFile;
+    String myData = "";
+    public Boolean isStoragePermissionGranted=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +55,68 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Action Required!!!", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//                getPrivateAlbumStorageDir(getApplicationContext(),"/hihasan folder2");
+                isExternalStorageWritable();
+                //isExternalStorageReadable();
             }
+
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            File file=new File(Environment.getExternalStorageState(),"/storage/extSdCard/Hakuna Matata");
+            if (!file.exists()){
+                file.mkdirs();
+                Toast.makeText(getApplicationContext(),"FOlder Created",Toast.LENGTH_SHORT).show();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"Folder Exists",Toast.LENGTH_SHORT).show();
+            }
             return true;
         }
+        Toast.makeText(getApplicationContext(),"Not Writeable",Toast.LENGTH_SHORT).show();
+        return false;
+    }
 
-        return super.onOptionsItemSelected(item);
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            Toast.makeText(getApplicationContext(),"Readable",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        Toast.makeText(getApplicationContext(),"Not Readable",Toast.LENGTH_SHORT).show();
+        return false;
+
+
+    }
+
+    public File getPrivateAlbumStorageDir(Context context, String albumName) {
+        // Get the directory for the app's private pictures directory.
+        File file = new File(context.getExternalFilesDir(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.exists()){
+            Log.e("Hihasan","FOlder Created");
+            file.mkdirs();
+            Toast.makeText(getApplicationContext(),"FOlder Created",Toast.LENGTH_SHORT).show();
+
+        }
+        else {
+            Log.e("Hihasan","Folder Exists");
+            Toast.makeText(getApplicationContext(),"Folder Exists",Toast.LENGTH_SHORT).show();
+        }
+//        if (!file.mkdirs()) {
+//            Log.e("Hihasan", "Directory not created");
+//        }
+        return file;
     }
 }
+
+
