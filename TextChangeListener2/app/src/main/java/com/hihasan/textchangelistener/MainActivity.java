f9fd1a096f80b8a79e38ebdd0afbf558205d57b4
hihasan <hasan@hihasan.com>
2019-12-30 17:49:36 +0600

    asdd

--- TextChangeListener2/app/src/main/java/com/hihasan/textchangelistener/MainActivity.java
+++ TextChangeListener2/app/src/main/java/com/hihasan/textchangelistener/MainActivity.java
@@ -6,46 +6,59 @@ import androidx.appcompat.widget.AppCompatEditText;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    AppCompatEditText volume,unit,total;
-
+    private TextWatcher tv1;
+    private TextWatcher tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initViews();
-    }
+        final EditText et1 = (EditText) findViewById(R.id.et1);
+        final EditText et2 = (EditText) findViewById(R.id.et2);
 
-    private void initViews()
-    {
-        volume = findViewById (R.id.volume);
-        unit= findViewById (R.id.unit);
-        total = findViewById (R.id.total);
 
-        volume.addTextChangedListener(new TextWatcher() {
+        tv1 = new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
             @Override
             public void afterTextChanged(Editable s) {
-
-                if (total.getText().toString().equalsIgnoreCase("0") && ){
-
-                }
-                unit.setText(s);
-                total.setText(s);
+                double x = Double.valueOf(et1.getText().toString());
+                et2.removeTextChangedListener(tv2); // "disable" the watcher
+                et2.setText(String.valueOf((x*2)));
+                et2.addTextChangedListener(tv2);    // "enable" the watcher
             }
-        });
+        };
+
+
+
+        tv2 = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                double x = Double.valueOf(et2.getText().toString());
+                et1.removeTextChangedListener(tv1); // "disable" the watcher
+                et1.setText(String.valueOf((x/2)));
+                et1.addTextChangedListener(tv1);    // "enable" the watcher
+            }
+        };
+
+        et1.addTextChangedListener(tv1);
+        et2.addTextChangedListener(tv2);
     }
 }
